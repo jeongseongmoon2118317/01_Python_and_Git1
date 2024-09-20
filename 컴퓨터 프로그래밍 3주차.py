9257caeaa709ef4f9f84fa6d7d24cd5dfdae481b
jeongseongmoon <b4jsm2@naver.com>
2024-09-20 22:04:21 +0900

    Create 컴퓨터 프로그래밍 3주차.py
    1. 인덱싱과 슬라이싱을 이용한 문제풀이
    2. 덧셈 후에 제곱하는 변수를 삽입함
    
    3. 슬라이싱과 인덱싱을 이용하여 result 변수에 할당하는 값을 구함
    
    4. 딕셔너리 변수를 이용하여 문제를 해결

---
+++ 컴퓨터 프로그래밍 3주차.py
@@ -0,0 +1,20 @@
+string = "Hello, World!"
+print(string[0:5])
+
+a = 7
+b = 3
+print(a + b)
+result = a + b
+print(result ** 2)
+
+a = [1, 2, 3, 4, 5]
+a.append(4)
+a.append(3)
+a.append(2)
+a.append(1)
+print(a)
+result = a[4:9]
+print(result)
+
+dic = {'apple': 3, 'banana': 5, 'cherry': 2}
+print(dic['banana'])
